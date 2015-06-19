99daea7772fb157517bfba49dc6b670aea4d16fc
Samuel Bandara <Samuel_Bandara@hms.harvard.edu>
2015-06-19 18:41:54 -0400

    Show titration trajectory for each drug.

--- drug_response_pca.m
+++ drug_response_pca.m
@@ -6,14 +6,12 @@ signals_647 = { 'pRSK_{Ser380}', 'pERK_{Thr202,Tyr204}', 'pAKT_{Ser473}', ...
     'Foxo3a', 'pS6_{Ser235,Ser236}', 'p4EBP1_{Thr37,46}', 'pCDK2_{Tyr15}', ...
     'pCDK1_{Tyr15}', 'pP57_{Thr310}', 'pP27_{Ser10}', 'pP27_{Thr187}', ...
     'Survivin', 'p27', 'p21', 'p57', 'FoxM1', 'CyclinB', 'CyclinA' };
-doses = [(10 * (3.1623.^(0:-1:-6)))*10^-6 0]; % drug concentration in M
-doses = doses(8:-1:1);
+doses = [0, (10 * (3.1623 .^ (-6 : 0))) * 10 ^ -6]; % drug conc in M
+n_doses = length(doses);
 cell_line = '10a';
 timepoint = '24h';
 idx_pRB = length(signals_647) + 1;
-drug_effects = zeros(length(drugs), idx_pRB);
-row_ctrl = 1;
-row_drug = 8;
+drug_effects = zeros(length(drugs), n_doses - 1, idx_pRB);
 ch_647 = 3;
 ch_568 = 5;
 
@@ -25,64 +23,68 @@ for drug_id = 1 : length(drugs)
         if mod(stain_id, 6) == 1
             % switch plates.
             plate_id = plate_id + 1;
-            col = 0;
+            col = 1;
         end
-        foldername = sprintf('%s_%s_%s_P%d/', cell_line, ...
+        folder = sprintf('%s_%s_%s_P%d/', cell_line, ...
             drugs{drug_id}, timepoint, plate_id);
-        col = col + 1;
-        % highest dose was applied to row 8, DMSO ro row 1. 
-        mn_ctrl = read_mean_stains(foldername, row_ctrl, col, ch_647);
-        mn_drug = read_mean_stains(foldername, row_drug, col, ch_647);
-        col = col + 1;
-        mn_ctrl = (mn_ctrl + read_mean_stains(foldername, row_ctrl, ...
-            col, ch_647)) / 2;
-        mn_drug = (mn_drug + read_mean_stains(foldername, row_drug, ...
-            col, ch_647)) / 2;
-        drug_effects(drug_id, stain_id) = mn_drug ./ mn_ctrl;
+        % DMSO was added to row 1.
+        mn_ctrl = read_stain_reps(folder, 1, [col, col + 1], ch_647);
+        for conc = 1 : n_doses - 1
+            mn_drug = read_stain_reps(folder, 1 + conc, [col, col + 1], ...
+                ch_647); 
+            drug_effects(drug_id, conc, stain_id) = mn_drug ./ mn_ctrl;
+        end
+        col = col + 2;
     end
     % augment effect matrix by drug effects on p-Rb.
-    mn_drug = 0;
-    mn_ctrl = 0;
+    mn_drug = zeros(n_doses, 1);
     for plate_id = 1 : 3
-        foldername = sprintf('%s_%s_%s_P%d/', cell_line, ...
-            drugs{drug_id}, timepoint, plate_id);
-        for col = 1 : 12
-            mn_drug = mn_drug + read_mean_stains(foldername, ...
-                row_drug, col, ch_568);
-            mn_ctrl = mn_ctrl + read_mean_stains(foldername, ...
-                row_ctrl, col, ch_568);
+        folder = sprintf('%s_%s_%s_P%d/', cell_line, drugs{drug_id}, ...
+            timepoint, plate_id);
+        for conc = 1 : n_doses
+            mn_drug(conc) = mn_drug(conc) + read_stain_reps(folder, ...
+                conc, 1 : 12, ch_568);
         end
     end
-    drug_effects(drug_id, idx_pRB) = mn_drug ./ mn_ctrl;
+    for conc = 2 : n_doses
+        drug_effects(drug_id, conc - 1, idx_pRB) = mn_drug(conc) ./ ...
+            mn_drug(1);
+    end
+    fprintf('finished analyzing stains for %s\n', drugs{drug_id});
 end
 save 10a_24h_drug_effects.mat
 
 %% PCA
 load 10a_24h_drug_effects.mat
-n_drugs = length(drugs);
 drug_effects = log2(drug_effects);
-mn_drug_effects = mean(drug_effects);
-drug_effects = drug_effects - repmat(mn_drug_effects, n_drugs, 1);
-[coeff, effects_pca, latent] = pca(drug_effects);
-offset = -mn_drug_effects * coeff;
-x = effects_pca(:, 1) - offset(1);
-y = effects_pca(:, 2) - offset(2);
-clf();
-plot (x, y, '*');
-for k = 1 : length(drugs)
-    text(x(k), y(k), drugs{k});
+n_drugs = length(drugs);
+n_sigs = length(signals_647) + 1;
+dfx = zeros(n_drugs * (n_doses - 1), n_sigs);
+for drug_id = 1 : n_drugs
+    dfx((1 : n_doses - 1) + (drug_id - 1) * (n_doses - 1), :) = ...
+        squeeze(drug_effects(drug_id, :, :));
 end
-idx_stain = [];
-for c = 1 : 2
-    idx_stain = unique([idx_stain; find(coeff(:, c) > 0.4)]);
+mn_dfx = mean(dfx);
+dfx = dfx - repmat(mn_dfx, n_drugs * (n_doses - 1), 1);
+coeff = pca(dfx);
+offset = -mn_dfx * coeff;
+sty = {'-ob', '-ok', '-om', '-^m', '-sm', '-or', '-^b', '-^k', '-^r'};
+for drug_id = 1 : length(drugs)
+    titr = squeeze(drug_effects(drug_id, :, :)) * coeff;
+    x = titr(:, 1) - offset(1);
+    y = titr(:, 2) - offset(2);
+    plot(x, y, sty{drug_id}), hold('on');
 end
-for a = idx_stain'
+idx_stain = find(sqrt(sum(coeff(:, [1, 2]) .^ 2, 2)) > 0.4);
+for sig = idx_stain'
     vec = zeros(19, 1);
-    vec(a) = -1;
+    vec(sig) = -1;
     vec = vec' * coeff;
     h = annotation('arrow');
     set(h, 'parent', gca(), 'position', [0, 0, vec(1), vec(2)]);
-    text(1.2 * vec(1), 1.2 * vec(2), signals_647{a});
+    text(1.2 * vec(1), 1.2 * vec(2), signals_647{sig});
 end
-xlim([-2.5, 0.5]);
-ylim([-1.25, 1]);
+xlim([-3.5, 0.5]);
+ylim([-1.5, 1]);
+legend(drugs);
+hold('off');
