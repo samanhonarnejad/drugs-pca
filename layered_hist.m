184ee2bd9e542501d65d77fe46d00eb292b36138
Samuel Bandara <Samuel_Bandara@hms.harvard.edu>
2015-07-17 12:08:20 -0400

    Add simple EGF dose response histograms for pRb and pS6.

---
+++ layered_hist.m
@@ -0,0 +1,89 @@
+%%
+load Organize_10a_Ligand_Response_Single_Cell_Data.mat;
+idx_pS6 = find(strcmp(signals_647, 'pS6_Ser235_236'));
+
+%% determine range for histograms
+n_egf_doses = 8;
+pRb_lo = inf;
+pRb_hi = -inf;
+pS6_lo = inf;
+pS6_hi = -inf;
+for dose = 1 : n_egf_doses
+    for r = 1 : 30
+        pRb = squeeze(single_cell_Alexa568_nuc_data(1, 1, dose, r, 1, :));
+        min_pRb = min(pRb);
+        if min_pRb < pRb_lo
+            pRb_lo = min_pRb;
+        end
+        max_pRb = prctile(pRb, 99);
+        if max_pRb > pRb_hi
+            pRb_hi = max_pRb;
+        end
+    end
+    pS6 = squeeze(single_cell_Alexa647_nuc_data(1, 1, dose, idx_pS6, 1, :));
+    min_pS6 = min(pS6);
+    if min_pS6 < pS6_lo
+        pS6_lo = min_pS6;
+    end
+    max_pS6 = prctile(pS6, 99);
+    if max_pS6 > pS6_hi
+        pS6_hi = max_pS6;
+    end
+end
+
+%% calculate histograms
+n_bin = 100;
+n_rep = 30;
+fn_log = @log2;
+log_rng_pRb = linspace(fn_log(pRb_lo), fn_log(pRb_hi), n_bin);
+log_rng_pS6 = linspace(fn_log(pS6_lo), fn_log(pS6_hi), n_bin);
+pRb = zeros(n_rep, n_bin, n_egf_doses);
+pS6 = zeros(n_bin, n_egf_doses);
+for dose = 1 : n_egf_doses
+    for rep = 1 : n_rep
+        rep_pRb = squeeze(single_cell_Alexa568_nuc_data(1, 1, dose, ...
+            rep, 1, :));
+        n = hist(fn_log(rep_pRb), log_rng_pRb);
+        pRb(rep, :, dose) = n ./ sum(n);
+    end
+    rep_pS6 = squeeze(single_cell_Alexa647_nuc_data(1, 1, dose, ...
+        idx_pS6, 1, :));
+    n = hist(fn_log(rep_pS6), log_rng_pS6);
+    pS6(:, dose) = n' ./ sum(n);
+end
+md_pRb = squeeze(median(pRb));
+
+%% plot histograms
+n = 1;
+for k = 2 : n_egf_doses
+    subplot(7, 2, 2 * n - 1);
+    plot(log_rng_pRb(1 : end - 1), md_pRb(1 : end - 1, 1), 'k');
+    hold on;
+    plot(log_rng_pRb(1 : end - 1), md_pRb(1 : end - 1, k), 'r');
+    hold off;
+    subplot(7, 2, 2 * n);
+    plot(log_rng_pS6(1 : end - 1), pS6(1 : end - 1, 1), 'k');
+    hold on;
+    plot(log_rng_pS6(1 : end - 1), pS6(1 : end - 1, k), 'r');
+    hold off;
+    n = n + 1;
+end
+
+%% 1D scatter plots
+for k = 1 : n_egf_doses
+    ss_pRb = fn_log(squeeze(single_cell_Alexa568_nuc_data(1, 1, k, 1, 1, :)));
+    ss_pRb(isnan(ss_pRb)) = [];
+    
+    pRb_idx = ss_pRb - log_rng_pRb(1);
+    pRb_idx = pRb_idx ./ (log_rng_pRb(end) - log_rng_pRb(1));
+    pRb_idx = round(pRb_idx * (n_bin - 1) + 1);
+    
+    exc_pRb = pRb_idx > 100;
+    ss_pRb(exc_pRb) = [];
+    pRb_idx(exc_pRb) = [];
+
+    xjit = randn(length(ss_pRb), 1) .* pRb(1, pRb_idx, k)';
+    subplot(1, 8, k);
+    plot(xjit, ss_pRb, '.');
+    ylim([7, 11]);
+end
